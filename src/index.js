31659fc051beac1ec2a3619c7932999751b87e54
henriquecatunda <henriquecatunda2@gmail.com>
2020-08-17 01:03:48 -0300

    add images

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Switch, Route } from  'react-router-dom';
+import App from './Home/App';
+import Portfolio from './Portfolio';
 
+const Pagina404 =() => (<div> Pagina 404 </div>)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>           
+      <Route path="/" component={App} exact/>
+      <Route path="/portfolio" component={Portfolio} exact/>        
+      <Route component={Pagina404}/> 
+    </Switch>
+</BrowserRouter>,
+
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
